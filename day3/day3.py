c04e0647e722611d8bfb5a46410b00d1b4f478e2
Laurens van der Kooi <laurens.van.der.kooi@rabobank.nl>
2022-12-03 07:45:53 +0100

    refactoring

--- day3/day3.py
+++ day3/day3.py
@@ -15,50 +15,56 @@ def get_letter_value(find_letter):
                 return i + 27
 
 
-# part 1
-with open(f"./puzzle-input-{day}.txt", "r") as file:
-    lines = file.readlines()
-    letter_map = {}
+def get_first_half(line):
+    current_line = line.strip()
+    line_length = int(len(current_line))
+    half = int(line_length / 2)
+
+    return current_line[0:half]
+
+
+def is_character_in_words(character, word1, word2):
+    return character in word1 and character in word2
+
+
+def part_one(lines):
     sum_values = 0
 
     for line in lines:
         letter_map = {}
-        current_line = line.strip()
-        line_length = int(len(current_line))
-        half = int(line_length / 2)
-
-        first_line = current_line[0:half]
-        second_line = current_line[half:line_length]
+        first_line = get_first_half(line)
+        second_line = line.replace(first_line, "")
 
         for character in first_line:
             if character in second_line:
                 letter_map[character] = get_letter_value(character)
 
         sum_values += sum(letter_map.values())
-    print(sum_values)
+    print(f'Total of part 1: {sum_values}')
 
-# part 2
 
-with open(f"./puzzle-input-{day}.txt", "r") as file:
-    lines = file.readlines()
-    group_map = {}
-    letter_map = {}
+def part_two(lines):
     sum_values = 0
     items_list = []
 
     for line in lines:
-        if len(items_list) <= 3:
-            items_list.append(line.strip())
-            if len(items_list) == 3:
+        items_list.append(line.strip())
 
-                for character in items_list[0]:
-                    if character in items_list[1] and character in items_list[2]:
-                        letter_map[character] = get_letter_value(character)
-                        break
-                sum_values += sum(letter_map.values())
-                items_list = []
-                letter_map = {}
-            else:
-                continue
+        if len(items_list) == 3:
 
-print(sum_values)
+            for character in items_list[0]:
+                if is_character_in_words(character, items_list[1], items_list[2]):
+                    sum_values += get_letter_value(character)
+                    break
+
+            items_list = []
+        else:
+            continue
+
+    print(f'Total of part 2: {sum_values}')
+
+
+with open(f"./puzzle-input-{day}.txt", "r") as file:
+    lines = file.readlines()
+    part_one(lines)
+    part_two(lines)
