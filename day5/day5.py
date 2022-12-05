914b0d52d27c7ef60b4f4021415b19594e8bf5be
laurensvanderkooi <lvvdkooi@gmail.com>
2022-12-05 21:18:19 +0100

    clean up

--- day5/day5.py
+++ day5/day5.py
@@ -1,57 +1,70 @@
 day = "day5"
 
-initial_stacks = {1: ['S', 'T', 'H', 'F', 'W', 'R'],
-                  2: ['S', 'G', 'D', 'Q', 'W'],
-                  3: ['B', 'T', 'W'],
-                  4: ['D', 'R', 'W', 'T', 'N', 'Q', 'Z', 'J'],
-                  5: ['F', 'B', 'H', 'G', 'L', 'V', 'T', 'Z'],
-                  6: ['L', 'P', 'T', 'C', 'V', 'B', 'S', 'G'],
-                  7: ['Z', 'B', 'R', 'T', 'W', 'G', 'P'],
-                  8: ['N', 'G', 'M', 'T', 'C', 'J', 'R'],
-                  9: ['L', 'G', 'B', 'W']}
 
-# part 1
-with open(f"./puzzle-input-{day}.txt", "r") as file:
-    lines = file.readlines()
+def get_initial_stacks():
+    return {1: ['S', 'T', 'H', 'F', 'W', 'R'],
+            2: ['S', 'G', 'D', 'Q', 'W'],
+            3: ['B', 'T', 'W'],
+            4: ['D', 'R', 'W', 'T', 'N', 'Q', 'Z', 'J'],
+            5: ['F', 'B', 'H', 'G', 'L', 'V', 'T', 'Z'],
+            6: ['L', 'P', 'T', 'C', 'V', 'B', 'S', 'G'],
+            7: ['Z', 'B', 'R', 'T', 'W', 'G', 'P'],
+            8: ['N', 'G', 'M', 'T', 'C', 'J', 'R'],
+            9: ['L', 'G', 'B', 'W']}
+
+
+def get_instructions_list(line):
+    return line.replace('move ', '').replace(' from ', ",").replace(' to ', ",").strip().split(',')
+
+
+def part_one(lines):
+    stacks = get_initial_stacks()
+    result = ''
 
     for line in lines:
-        line.strip()
-        instructions = line.replace('move ', '').replace(' from ', ",").replace(' to ', ",").strip().split(',')
+        instructions = get_instructions_list(line)
         number_of_containers = int(instructions[0])
         from_container = int(instructions[1])
         to_container = int(instructions[2])
 
         container_list = []
         for number in range(0, number_of_containers):
-            container_list.append(initial_stacks[from_container].pop())
+            container_list.append(stacks[from_container].pop())
 
         for container in container_list:
-            initial_stacks[to_container].append(container)
+            stacks[to_container].append(container)
+
+    for stack in stacks.values():
+        result += stack.pop()
+
+    print(f'Result part 1: {result}')
 
 
-    for stack in initial_stacks.values():
-        print(stack.pop())
-
-# part 2
-with open(f"./puzzle-input-{day}.txt", "r") as file:
-    lines = file.readlines()
+def part_two(lines):
+    stacks = get_initial_stacks()
+    result = ''
 
     for line in lines:
-        line.strip()
-        instructions = line.replace('move ', '').replace(' from ', ",").replace(' to ', ",").strip().split(',')
+        instructions = get_instructions_list(line)
         number_of_containers = int(instructions[0])
         from_container = int(instructions[1])
         to_container = int(instructions[2])
 
-        container_list = initial_stacks[from_container][-number_of_containers:]
+        container_list = stacks[from_container][-number_of_containers:]
 
         for number in range(0, number_of_containers):
-         initial_stacks[from_container].pop()
+            stacks[from_container].pop()
 
         for container in container_list:
-            initial_stacks[to_container].append(container)
+            stacks[to_container].append(container)
 
-    print(initial_stacks)
+    for stack in stacks.values():
+        result += stack.pop()
 
-    for stack in initial_stacks.values():
-        print(stack.pop())
+    print(f'Result part 2: {result}')
+
+
+with open(f"./puzzle-input-{day}.txt", "r") as file:
+    lines = file.readlines()
+    part_one(lines)
+    part_two(lines)
