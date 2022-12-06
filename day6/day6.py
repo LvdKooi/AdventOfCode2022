565e3ed4acac0b49f482e8053d9837f7af529c62
Laurens van der Kooi <laurens.van.der.kooi@rabobank.nl>
2022-12-06 09:39:52 +0100

    clean up

--- day6/day6.py
+++ day6/day6.py
@@ -1,34 +1,29 @@
 day = 'day6'
 
 
-def check_more_than_one_occurence(number):
-    if number > 1:
-        return True
-
-    return False
-
-
-with open(f"./puzzle-input-{day}.txt", "r") as file:
-    line = file.readlines()[0]
-
-    print(line)
-
+def part_one_and_two(line, number_of_unique_characters, log_statement):
     character_list = []
 
     for i, character in enumerate(line):
         character_list.append(character)
 
-        if len(character_list) == 14:
+        if len(character_list) == number_of_unique_characters:
             count_map = {}
             for item in character_list:
                 count_map[item] = character_list.count(item)
 
-            even_numbers_iterator = filter(check_more_than_one_occurence, count_map.values())
+            number_of_double_occurences = len(list(filter(lambda number: number > 1, count_map.values())))
 
-            number_of_double_occurences = len(list(even_numbers_iterator))
             if number_of_double_occurences == 0:
-                print(i + 1)
+                print(f'{log_statement}{i + 1}')
                 break
 
             character_list.pop(0)
             continue
+
+
+with open(f"./puzzle-input-{day}.txt", "r") as file:
+    line = file.readlines()[0]
+
+    part_one_and_two(line, 4, 'Result part 1: ')
+    part_one_and_two(line, 14, 'Result part 2: ')
