e159ed27259304162487e0a80446f932e2594df3
Laurens van der Kooi <laurens.van.der.kooi@rabobank.nl>
2022-12-07 11:10:08 +0100

    solution day 7

--- day7/day7.py
+++ day7/day7.py
@@ -1,31 +1,21 @@
 day = 'day7'
 
-directory_map = {}
 
-with open(f"./puzzle-input-{day}.txt", "r") as file:
-    lines = file.readlines()
-
-    current_dir = '/'
-    current_path = []
+def get_count_per_directory(lines):
+    directory_map = {}
+    current_path = list()
 
     for i, line in enumerate(lines):
-
         if '$ cd ' in line:
             if ' ..' in line:
                 if len(current_path) > 1:
                     current_path.pop()
             else:
                 new_dir = line.replace('$ cd ', '').strip()
-
-                if new_dir in current_path:
-                    while new_dir != current_path.pop():
-                        continue
-                else:
-                    current_path.append(new_dir)
+                current_path.append(new_dir)
 
         if line[0].isdigit():
             size = line.split(' ')[0]
-
             dir_string = ''
             for dir in current_path:
                 dir_string += dir + "/"
@@ -35,5 +25,22 @@ with open(f"./puzzle-input-{day}.txt", "r") as file:
                 else:
                     directory_map[dir_string] = int(size)
 
-    print(directory_map)
+    return directory_map
+
+
+def part_one(directory_map):
     print(sum(list(filter(lambda number: number <= 100000, directory_map.values()))))
+
+
+def part_two(directory_map):
+    max_size = 70000000
+    current_size = max(directory_map.values())
+
+    print(min(list(filter(lambda number: (max_size - (current_size - number)) >= 30000000, directory_map.values()))))
+
+
+with open(f"./puzzle-input-{day}.txt", "r") as file:
+    lines = file.readlines()
+    count_per_directory = get_count_per_directory(lines)
+    part_one(count_per_directory)
+    part_two(count_per_directory)
