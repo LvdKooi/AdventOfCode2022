937392c5138290aafeb75ea731ba325df1f3244b
Laurens van der Kooi <laurens.van.der.kooi@rabobank.nl>
2022-12-08 18:26:48 +0100

    cleaning up, still horrible code

--- day8/day8.py
+++ day8/day8.py
@@ -3,27 +3,27 @@ day = 'day8'
 # #  part 1
 with open(f"./puzzle-input-{day}.txt", "r") as file:
     lines = file.readlines()
-    outside_count = (len(lines[0].strip()) * 2) + ((len(lines) - 2) *2)
+    outside_count = (len(lines[0].strip()) * 2) + ((len(lines) - 2) * 2)
     number_of_lines = len(lines)
     number_of_characters = len(lines[0].strip())
     total_count = outside_count
 
-    for i in range(1, number_of_lines -1):
+    for i in range(1, number_of_lines - 1):
 
         for j in range(1, number_of_characters - 1):
             current_number = lines[i][j]
-            number_of_trees_larger_from_top = 0
-            number_of_trees_larger_from_bottom = 0
-            number_of_trees_larger_from_left = 0
-            number_of_trees_larger_from_right = 0
+            top_count = 0
+            bottom_count = 0
+            left_count = 0
+            right_count = 0
 
             # visible from top
             for k in range(0, i):
                 compare = lines[k][j]
                 if compare >= current_number:
-                    number_of_trees_larger_from_top += 1
+                    top_count += 1
 
-            if number_of_trees_larger_from_top == 0:
+            if top_count == 0:
                 total_count += 1
                 continue
 
@@ -31,9 +31,9 @@ with open(f"./puzzle-input-{day}.txt", "r") as file:
             for k in range(i + 1, number_of_lines):
                 compare = lines[k][j]
                 if compare >= current_number:
-                    number_of_trees_larger_from_bottom += 1
+                    bottom_count += 1
 
-            if number_of_trees_larger_from_bottom == 0:
+            if bottom_count == 0:
                 total_count += 1
                 continue
 
@@ -41,9 +41,9 @@ with open(f"./puzzle-input-{day}.txt", "r") as file:
             for k in range(0, j):
                 compare = lines[i][k]
                 if compare >= current_number:
-                    number_of_trees_larger_from_left += 1
+                    left_count += 1
 
-            if number_of_trees_larger_from_left == 0:
+            if left_count == 0:
                 total_count += 1
                 continue
 
@@ -51,74 +51,70 @@ with open(f"./puzzle-input-{day}.txt", "r") as file:
             for k in range(j + 1, number_of_characters):
                 compare = lines[i][k]
                 if compare >= current_number:
-                    number_of_trees_larger_from_right += 1
+                    right_count += 1
 
-            if number_of_trees_larger_from_right == 0:
+            if right_count == 0:
                 total_count += 1
                 continue
 
     print(total_count)
 
-
 #  part 2
 with open(f"./puzzle-input-{day}.txt", "r") as file:
     lines = file.readlines()
-    outside_count = (len(lines[0].strip()) * 2) + ((len(lines) - 2) * 2)
     number_of_lines = len(lines)
     number_of_characters = len(lines[0].strip())
-    total_count = outside_count
     scenic_view_counts = []
 
     for i in range(1, number_of_lines - 1):
 
         for j in range(1, number_of_characters - 1):
             current_number = lines[i][j]
-            number_of_trees_larger_from_top = 0
-            number_of_trees_larger_from_bottom = 0
-            number_of_trees_larger_from_left = 0
-            number_of_trees_larger_from_right = 0
+            top_count = 0
+            bottom_count = 0
+            left_count = 0
+            right_count = 0
 
             # visible from top
             for k in range(i - 1, -1, -1):
                 compare = lines[k][j]
                 if compare < current_number:
-                    number_of_trees_larger_from_top += 1
+                    top_count += 1
 
                 if compare >= current_number:
-                    number_of_trees_larger_from_top += 1
+                    top_count += 1
                     break
 
             # visible from bottom
             for k in range(i + 1, number_of_lines):
                 compare = lines[k][j]
                 if compare < current_number:
-                    number_of_trees_larger_from_bottom += 1
+                    bottom_count += 1
 
                 if compare >= current_number:
-                    number_of_trees_larger_from_bottom += 1
+                    bottom_count += 1
                     break
 
-                    # visible from left
+            # visible from left
             for k in range(j - 1, -1, -1):
                 compare = lines[i][k]
                 if compare < current_number:
-                    number_of_trees_larger_from_left += 1
+                    left_count += 1
 
                 if compare >= current_number:
-                    number_of_trees_larger_from_left += 1
+                    left_count += 1
                     break
 
-                    # visible from right
+            # visible from right
             for k in range(j + 1, number_of_characters):
                 compare = lines[i][k]
                 if compare < current_number:
-                    number_of_trees_larger_from_right += 1
+                    right_count += 1
 
                 if compare >= current_number:
-                    number_of_trees_larger_from_right += 1
+                    right_count += 1
                     break
 
-            scenic_view_counts.append(
-                number_of_trees_larger_from_left * number_of_trees_larger_from_right * number_of_trees_larger_from_bottom * number_of_trees_larger_from_top)
+            scenic_view_counts.append(left_count * right_count * bottom_count * top_count)
 
     print(max(scenic_view_counts))
